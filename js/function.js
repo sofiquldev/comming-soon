d4b13820d7ff4109357ec5f3d3bdb656fec965fa
sofiquldev <sofiquldev@gmail.com>
2023-11-07 09:42:42 +0600

    first commit

---
+++ js/function.js
@@ -0,0 +1,20 @@
+(function ($) {
+    "use strict";
+	
+	var $window = $(window); 
+	
+	/* Typed subtitle */
+	$('.typed-title').typed({
+		stringsElement: $('.typing-title'),
+		backDelay: 2000,
+		typeSpeed: 60,
+		loop: true
+	});
+
+    if($('#comming-soon-subscriber').length) {
+        $('#comming-soon-subscriber').on('submit', function() {
+            alert('Thanks for submission our contact form. We\'ll rich you soon.')
+        })
+    }
+
+})(jQuery);
